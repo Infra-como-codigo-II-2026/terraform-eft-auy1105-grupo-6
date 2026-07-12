f2a76e418dd22247480b53639173f06b5277a4e0
EC2 Default User <ec2-user@ip-172-31-5-128.ec2.internal>
2026-07-12 02:49:00 +0000

    feat: agrega pipeline separado para el bootstrap del backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket       = "auy1105-prod-tfstate-grupo-6"
-    key          = "eft/grupo-6/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
+  backend "s3" {}
 }
