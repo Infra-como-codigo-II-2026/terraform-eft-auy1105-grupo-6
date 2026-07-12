b372aefab28636973d521a6ed562d61104fd8fc3
EC2 Default User <ec2-user@ip-172-31-5-128.ec2.internal>
2026-07-12 02:03:37 +0000

    fix: configura backend remoto con bucket real del bootstrap

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "REEMPLAZAR-con-output-state_bucket_name-de-bootstrap"
-    key            = "eft/grupo-6/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "REEMPLAZAR-con-output-lock_table_name-de-bootstrap"
+    bucket       = "auy1105-prod-tfstate-grupo-6"
+    key          = "eft/grupo-6/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
